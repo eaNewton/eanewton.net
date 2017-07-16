4de8a3b7ad10258fb5f67ecf36853a3ec9e347e7
e. a. newton <emmettnewton1@gmail.com>
2017-07-16 16:09:53 -0500

    switch html to php, add splash construction w/ styling

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>eanewton.net</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.min.css">
+  </head>
+  <body>
+	   <div class="canvas">
+       <div class="container">
+         <div class="header"></div>
+         <div class="content">
+           <h1 id="under_construction">Under Construction.<span id="again">(...again)</span></h1>
+         </div>
+         <footer class="footer">
+           <p id="copyright">&copy; <?php echo date("Y"); ?></p>
+         </footer>
+       </div>
+     </div>
+  </body>
+</htm
