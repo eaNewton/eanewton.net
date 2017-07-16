7aacaebfb2fcff210ac3e26c1488eb78849f7579
e. a. newton <emmettnewton1@gmail.com>
2017-07-16 15:01:36 -0500

    old site purge

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    size = require('gulp-size'),
+    stripDebug = require('gulp-strip-debug'),
+    sourcemaps = require('gulp-sourcemaps'),
+    pkg = require('./package.json');
+
+var paths = {
+  styles: './css/sass/styles.scss',
+  scripts: ['./js/main.js']
+};
+
+gulp.task('styles', ['components'], function() {
+  return sass(paths.styles, { sourcemap: false })
+  .on('error', function (err) {
+    console.error('Error', err.message);
+  })
+  .pipe(rename('app.css'))
+  .pipe(gulp.dest('./css/'))
+  .pipe(rename('app.min.css'))
+  .pipe(minifyCSS())
+  .pipe(sourcemaps.write())
+  .pipe(size())
+  .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('components', function() {
+  return gulp.src(['./components/normalize.css/normalize.css'])
+  .pipe(rename('_normalize.scss'))
+  .pipe(gulp.dest('./components/normalize.css/'));
+});
+
+gulp.task('scripts', ['lint'], function() {
+  gulp.src(paths.scripts)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./js/'))
+    .pipe(rename('app.min.js'))
+
+    //.pipe(stripDebug())
+    .pipe(uglify({ preserveComments: 'some' }))
+    .pipe(size())
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('lint', function () {
+  return gulp.src(['./js/main.js'])
+    .pipe(jshint('./.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['styles', 'scripts']);
