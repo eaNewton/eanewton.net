e7bc4099e88d40b0707458b5cde0cb4f670909dc
e. a. newton <emmettnewton1@gmail.com>
2017-07-17 22:35:02 -0500

    added monkee-mixins, beginning form styles/config

--- contact.php
+++ contact.php
@@ -5,7 +5,12 @@
 ?>
 <div class="container">
     <div class="content">
-      <?php include($_SERVER["DOCUMENT_ROOT"] . "/resources/library/contact-form.php") ?>
+      <?php
+      ini_set('display_errors', 1);
+      error_reporting(E_ALL|E_STRICT);
+
+      include($_SERVER["DOCUMENT_ROOT"] . "/resources/library/contact-form.php");
+      ?>
     </div>
 </div>
 <?php
--- resources/library/contact-form.php
+++ resources/library/contact-form.php
@@ -1,20 +1,66 @@
+<?php
+//Vars
+$nameError = $emailError = "";
+$name = $email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameError = "Your name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    //Make sure name only contains letters & whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+      $nameError = "Only letters + spaces";
+    }
+  }
+
+  if (empty($_POST["email"])) {
+    $emailError = "Your email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    //Make sure email is a valid email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailError = "Invalid email";
+    }
+  }
+
+  if (empty($_POST["message"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["message"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+
 <div class="form-container">
-  <span class="input input--yoshiko">
-  	<input class="input__field input__field--yoshiko" type="text" id="input-10" />
-  	<label class="input__label input__label--yoshiko" for="input-10">
-  		<span class="input__label-content input__label-content--yoshiko" data-content="Name">Name</span>
-  	</label>
-  </span>
-  <span class="input input--yoshiko">
-  	<input class="input__field input__field--yoshiko" type="text" id="input-11" />
-  	<label class="input__label input__label--yoshiko" for="input-11">
-  		<span class="input__label-content input__label-content--yoshiko" data-content="Email">Email</span>
-  	</label>
-  </span>
-  <span class="input input--yoshiko">
-  	<input class="input__field input__field--yoshiko" type="text" id="input-12" />
-  	<label class="input__label input__label--yoshiko" for="input-12">
-  		<span class="input__label-content input__label-content--yoshiko" data-content="Message">Message</span>
-  	</label>
-  </span>
+  <form id="contact-form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <div class="form_field" id="name_field">
+      <label for="name">name</label>
+      <input type="text" name="name" value="<?php echo $name; ?>"></input>
+    </div>
+    <div class="form_field" id="email_field">
+      <label for="email">email</label>
+      <input type="email" name="email" value="<?php echo $email; ?>"></input>
+    </div>
+    <div class="form_field" id="message_field">
+      <label for="message">message</label>
+      <textarea name="message" rows="5" columns="40"></textarea>
+    </div>
+    <input id="submit-button" type="submit" name="submit" value="submit"></input>
+  </form>
 </div>
+
+<?php
+
+echo $name;
+echo $email;
+
+?>
